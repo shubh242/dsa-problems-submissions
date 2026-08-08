66aabf916d8a912f0b8d9a3b8dc6b54bf0d77cdd
shubh242 <64204436+shubh242@users.noreply.github.com>
2026-08-08 03:53:07 -0700

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String smallest = "a";
+        int smallestLength = Integer.MAX_VALUE;
+        int smallestIdx = 0;
+
+        for(int i = 0; i < strs.length; i++) {
+            if(smallestLength > strs[i].length()) {
+                smallest = strs[i];
+                smallestLength = strs[i].length();
+                smallestIdx = i;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < smallest.length(); i++) {
+            sb.append(smallest.charAt(i));
+            for(int j = 0; j < strs.length; j++) {
+                String prefix = sb.toString();
+                if(smallestIdx == j)
+                    continue;
+
+                if(!strs[j].startsWith(prefix)) {
+                    sb.deleteCharAt(sb.length()-1);
+                    System.out.println(sb.toString());
+                    return sb.toString();
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
