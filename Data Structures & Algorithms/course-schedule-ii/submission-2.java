788e538289044bd24c338cde5376b0282a426415
shubh242 <64204436+shubh242@users.noreply.github.com>
2026-09-14 21:01:20 -0700

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, ArrayList<Integer>> adj = new HashMap<>();
+        int[] indegree = new int[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+        int coursesTaken = 0;
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i < numCourses; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+
+        for(int i = 0; i < prerequisites.length; i++) {
+            ArrayList<Integer> tmp = adj.get(prerequisites[i][0]);
+            tmp.add(prerequisites[i][1]);
+            adj.put(prerequisites[i][0], tmp);
+            indegree[prerequisites[i][1]]++;
+        }
+
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0)
+                q.add(i);
+        }
+
+        while(!q.isEmpty()) {
+            int course = q.poll();
+            result.add(course);
+            coursesTaken++;
+            if(adj.get(course).size() > 0) {
+                for(Integer nei: adj.get(course)) {
+                    indegree[nei]--;
+                    if(indegree[nei] == 0) {
+                        q.add(nei);
+                    }
+                }
+            } else {
+                adj.remove(course);
+            }
+        }
+        if(numCourses != coursesTaken)
+            return new int[0];
+
+        int[] res = new int[result.size()];
+        for(int i = result.size()-1; i >= 0; i--) {
+            res[numCourses - i - 1] = result.get(i);
+        }
+        return res;
+    }
+}
